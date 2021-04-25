22cd6fe00f8f1795aaa2190d4f8e41eb14093ab0
RVadim <roibu.vadim@gmail.com>
2021-04-25 21:22:11 +0300

    final project

---
+++ README.md
@@ -0,0 +1,15 @@
+# DevProjects - Link Shortener Website
+This is an open source project from [DevProjects](http://www.codementor.io/projects). Feedback and questions are welcome!
+Find the project requirements here: [Link shortener website](https://www.codementor.io/projects/web/link-shortener-website-brqjanf6zq)
+
+## Tech/framework used
+Built with HTML/CSS and JavaScript
+
+## Screenshots and demo
+https://link-shortener-rv7.herokuapp.com/
+
+## Installation
+Replace the bitly API token in `script.js` with your own.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ script.js
@@ -0,0 +1,67 @@
+const BITLY_API_ACCESS_TOKEN = 'YOUR_API' // Paste your bitly token here!!!
+const inputForm = document.querySelector('.input--form input')
+const formButton = document.querySelector('.input button')
+const result = document.querySelector('.response--p')
+const copyButton = document.querySelector('.copy-button')
+const resultDiv = document.querySelector('.response--text')
+const errorMsg = document.querySelector('.error-msg')
+
+formButton.addEventListener('click', function(){
+    if (inputForm.value) {
+        shortURL(inputForm.value)
+    }
+    else {
+        errorMsg.style.display = 'block'
+    }
+})
+
+copyButton.addEventListener('click', function(){
+    copyElementText(result)
+})
+
+function shortURL(input) {fetch('https://api-ssl.bitly.com/v4/shorten', {
+    method: 'POST',
+    headers: {
+        'Authorization': `Bearer ${BITLY_API_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "long_url": input, "domain": "bit.ly"})
+})
+    .then(data => {
+        if (data.status !== 200) {
+            if (resultDiv.style.display !== 'none'){
+                hideResult()
+                showError(data)
+            }
+            showError(data)
+        }
+        return data.json()})
+    .then(response => 
+        {if (errorMsg.style.display !== 'none'){
+            errorMsg.style.display = 'none'}
+            result.innerHTML = response.link
+            showResult()
+        })
+    .catch(e => console.error(e))
+}
+
+function showResult() {
+    resultDiv.style.display = 'block'
+    copyButton.style.display = 'block'
+}
+
+function hideResult() {
+    resultDiv.style.display = 'none'
+    copyButton.style.display = 'none'
+}
+
+function copyElementText(id) {
+    var text = id.innerText;
+    navigator.clipboard.writeText(text)
+}
+
+function showError(data) {
+    errorMsg.style.display = 'block'
+    inputForm.value = ''
+    throw new Error ('Somethink went wrong! HTTP error: ' + data.status)
+}
